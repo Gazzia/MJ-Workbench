47aa47dcd509a338c0911aed4e03667671cf87aa
Joseph Allain <joseph.allain24@gmail.com>
2021-07-03 18:42:42 +0200

    reworked login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {AngularFireAuth} from "@angular/fire/auth";
 import {FormControl, Validators} from "@angular/forms";
 import {environment} from "../../environments/environment"
 import {Router} from "@angular/router";
+import {faTimes, faCheck} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-login',
@@ -13,18 +14,21 @@ export class LoginComponent {
   passwordField = new FormControl("", [Validators.required])
   errorMessage: string | null = null;
 
+  faTimes = faTimes;
+  faCheck = faCheck;
+
   constructor(public auth: AngularFireAuth, private router: Router) {
+    this.auth.signOut();
   }
 
   async login() {
     this.errorMessage = null;
-    await this.router.navigate(["/library"])
     const pass = this.passwordField.value
     const mail = environment.email
     this.auth.signInWithEmailAndPassword(mail, pass)
       .then((user) => {
         console.log(user)
-
+        this.goToLibrary()
       })
       .catch(error => {
         if (error.code == "auth/wrong-password"){
@@ -32,5 +36,7 @@ export class LoginComponent {
         }
       });
   }
+
+  goToLibrary = async () => await this.router.navigate(["library"])
 
 }
